c05dde4660098ace335b2cccc4e1f78fcf08c16f
cpage2 <mcpage25@gmail.com>
2022-12-09 14:25:58 -0600

    Added key event to lp tasks and pickButton

--- tpScript.js
+++ tpScript.js
@@ -8,8 +8,12 @@ trash.className = 'fa-solid fa-trash-can';
 
 
 const hpInput = document.querySelector('#hp'); // hp input value
-
 const hpItem = document.querySelector('#hpTask');//list item
+
+const lpInput = document.querySelector('#lp'); // hp input value
+const lpItem = document.querySelector('#lpTask');//list item
+
+
 
 
 hpItem.addEventListener("keypress", (e)=>{
@@ -20,13 +24,9 @@ hpItem.addEventListener("keypress", (e)=>{
         
         let newHPItem = document.createElement("li");
         
-        newHPItem.innerHTML = `${hpInput.value}     ${trash.innerHTML}`;
+        newHPItem.innerHTML = hpInput.value;
         hpItem.appendChild(newHPItem);//adds new list item to ul
         
-       
-       
-       
-
        
         // Add input to HP Array
         hpArray.push(hpInput.value);
@@ -41,4 +41,55 @@ hpItem.addEventListener("keypress", (e)=>{
 
 )
 
+lpItem.addEventListener("keypress", (e)=>{
+    if(e.key === "Enter"){
+        // Display text
+        // Add new li/input item with trash icon
+        // then new event onclick for ticon
+        
+        let newLPItem = document.createElement("li");
+        
+        newLPItem.innerHTML = lpInput.value;
+        lpItem.appendChild(newLPItem);//adds new list item to ul
+        
+       
+        // Add input to HP Array
+        lpArray.push(lpInput.value);
 
+       
+        lpInput.value = "";//resets input value
+        
+
+
+    }
+}
+
+)
+
+
+// Pickr
+
+const pickButton = document.querySelector('.pick');
+
+function getRandIndex(x){
+    return Math.floor(Math.random() * x);
+}
+
+
+pickButton.addEventListener('click',(e)=>{
+    // Randomly pick a hp and lp task
+    // Display them on second card in order of hp
+
+    let hpPick = hpArray[getRandIndex(hpArray.length)];
+    let lpPick = lpArray[getRandIndex(lpArray.length)];
+
+    const item1 = document.querySelector('#item1');//li item 1
+    const item2 = document.querySelector('#item2');// list item 2
+
+    item1.innerHTML = hpPick + ' !';
+    item2.innerHTML = lpPick;
+
+   
+
+
+})
