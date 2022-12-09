6e562b97b9bd34f7458a2689067d28c136f67f8b
cpage2 <mcpage25@gmail.com>
2022-12-08 20:19:50 -0600

    Add keypress event fo hp tasks

--- tpScript.js
+++ tpScript.js
@@ -1,5 +1,44 @@
 // Create variables
 
-const hpList = []; //high priority tasks
-const lpList = []; // low priority tasks
+const hpArray = []; //high priority tasks
+const lpArray = []; // low priority tasks
+const trash = document.createElement('i');
+trash.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+trash.className = 'fa-solid fa-trash-can';
+
+
+const hpInput = document.querySelector('#hp'); // hp input value
+
+const hpItem = document.querySelector('#hpTask');//list item
+
+
+hpItem.addEventListener("keypress", (e)=>{
+    if(e.key === "Enter"){
+        // Display text
+        // Add new li/input item with trash icon
+        // then new event onclick for ticon
+        
+        let newHPItem = document.createElement("li");
+        
+        newHPItem.innerHTML = `${hpInput.value}     ${trash.innerHTML}`;
+        hpItem.appendChild(newHPItem);//adds new list item to ul
+        
+       
+       
+       
+
+       
+        // Add input to HP Array
+        hpArray.push(hpInput.value);
+
+       
+        hpInput.value = "";//resets input value
+        
+
+
+    }
+}
+
+)
+
 
